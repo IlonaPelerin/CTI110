719296efa3a27b126cf29e489ab4383871af1422
IlonaPelerin <31409420+IlonaPelerin@users.noreply.github.com>
2017-08-28 13:04:04 -0400

    Add files via upload
    M2HW1 Distance Traveled

---
+++ M2HW1_DistanceTraveled_Pelerin.py
@@ -0,0 +1,19 @@
+#CTI-110
+#M2HW1 - Distance Traveled
+#Ilona Pelerin
+#August 28,2017
+#
+#The distance traveled is equal to speed * time.  The speed is set at 70 mph.
+speed = 70
+
+#The distance traveled after 6 hours and its print out.
+distanceAfter6 = speed * 6
+print("The distance the car will travel in six hours is: ", distanceAfter6)
+
+#The distance traveled after10 hoursand and its print out.
+distanceAfter10 = speed * 10
+print("The distance the car will travel in ten hours is: ", distanceAfter10)
+
+#The distance traveled after 15 hours and its print out.
+distanceAfter15 = speed * 15
+print("The distance the car will travel in fifteen hours is: ", distanceAfter15)
